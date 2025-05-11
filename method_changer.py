bb78a183df132200c88d6d2c476348ea4a618ce8
HipstorSmith <svinkachu@protonmail.com>
2025-05-12 02:01:59 +0300

    vec_changer.py added docstrings method_changer.py added

--- method_changer.py
+++ method_changer.py
@@ -3,33 +3,30 @@ import os
 from glob import glob
 
 
-def vec_changer(input_file, vec_file, output_file):
+def method_changer(input_file, method_file, output_file):
     """
-    Change $VEC group in the input_file
+    Change header in the input_file (everything except $DATA and $VEC groups)
     :param input_file: input file with .inp extension
-    :param vec_file: file with replacement $VEC group
+    :param method_file: file with replacement header
     :param output_file: output .inp file
     :return:
     """
 
-    # Read input file except $VEC group
+    # Read new header
     output_lines = []
-    with open(input_file, encoding='utf8') as f:
-        for line in f:
-            if 'VEC' in line:
-                break
-            output_lines.append(line)
+    with open(method_file, encoding='utf8') as f:
+        output_lines.extend(line for line in f)
+    data_found = False
 
     if not output_lines[-1].endswith('\n'):
         output_lines[-1] += '\n'
 
-    # Read $VEC group
-    vec_found = False
-    with open(vec_file, encoding='utf8') as f:
+    # Read read input file after header ($DATA and $VEC groups)
+    with open(input_file, encoding='utf8') as f:
         for line in f:
-            if not vec_found:
-                if 'VEC' in line:
-                    vec_found = True
+            if not data_found:
+                if 'DATA' in line:
+                    data_found = True
                     output_lines.append(line)
             else:
                 output_lines.append(line)
@@ -38,14 +35,14 @@ def vec_changer(input_file, vec_file, output_file):
         f.writelines(output_lines)
 
 
-def main(input_path, vec_file, output_path):
+def main(input_path, method_file, output_path):
     if os.path.isdir(input_path):
         for input_file in glob(os.path.join(input_path, '*.inp')):
             output_file = os.path.join(output_path, os.path.basename(input_file))
-            vec_changer(input_file, vec_file, output_file)
+            method_changer(input_file, method_file, output_file)
     elif (ext := os.path.splitext(input_path)) == '.inp':
         output_file = os.path.join(output_path, os.path.basename(input_path))
-        vec_changer(input_path, vec_file, output_file)
+        method_changer(input_path, method_file, output_file)
     else:
         raise TypeError(f'Unknown file format {ext}')
 
@@ -53,7 +50,7 @@ def main(input_path, vec_file, output_path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_path', help='Path to the folder with .inp files or single .inp file to process')
-    parser.add_argument('--vec_file', help='Path to the vec file')
+    parser.add_argument('--method_file', help='Path to the method file (header)')
     parser.add_argument('--output_path', help='Path to the output folder')
     args = parser.parse_args()
     main(**vars(args))
